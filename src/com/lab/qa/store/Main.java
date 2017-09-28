44b73ec465099ef119447e1dcd9767040905f8ef
Svarus <kyryasirko@gmail.com>
2017-09-28 08:52:18 +0300

    Initial commit

---
+++ src/com/lab/qa/store/Main.java
@@ -0,0 +1,10 @@
+package com.lab.qa.store;
+
+import com.lab.qa.store.models.Product;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Product product;
+    }
+}
---
+++ src/com/lab/qa/store/models/Product.java
@@ -0,0 +1,4 @@
+package com.lab.qa.store.models;
+
+public interface Product {
+}
